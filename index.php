388f3d98817f37c3d63455942224ec4454a2e3e7
leonatefm <chengqizhufm@gmail.com>
2013-12-03 17:13:43 -0500

    mainbar mockup

--- index.php
+++ index.php
@@ -17,6 +17,114 @@
 </head>
 <body>
 	<?php require_once("header.php"); ?>
+	<div class="container">
+		<div class="row">
+			<div id="leftbar" class="col-md-2">
+			</div>
+			<div id="mainbar" class="col-md-7">
+				<h3>Task Explore</h3>
+				<ul id="tasklist" class="list-unstyled">
+					<li>
+						<div class="author"><img src="img/avatar.jpg" class="avatar img-rounded"></div>
+						<div class="content">
+							<h5>Name</h5>
+							<h4>Ok, so here would be the title of the task</h4>
+							<p>Be quiet, the adults are testing!!!</p>
+							<div class="information">
+								<div class="infoleft"><h4><small>Likes:15</small></h4></div>
+								<div class="infoleft"><h4><small>Comments:9</small></h4></div>
+								<div class="infoleft"><h4><small>Applied:5</small></h4></div>
+								<div class="inforight"><h4><small>Time: Just Now</small></h4></div>
+							</div>
+						</div>
+					</li>
+					<li>
+						<div class="author"><img src="img/avatar.jpg" class="avatar img-rounded"></div>
+						<div class="content">
+							<h5>Name</h5>
+							<h4>Ok, so here would be the title of the task</h4>
+							<p>Be quiet, the adults are testing!!!</p>
+							<div class="information">
+								<div class="infoleft"><h4><small>Likes:15</small></h4></div>
+								<div class="infoleft"><h4><small>Comments:9</small></h4></div>
+								<div class="infoleft"><h4><small>Applied:5</small></h4></div>
+								<div class="inforight"><h4><small>Time: Yesterday</small></h4></div>
+							</div>
+						</div>
+					</li>
+					<li>
+						<div class="author"><img src="img/avatar.jpg" class="avatar img-rounded"></div>
+						<div class="content">
+							<h5>Name</h5>
+							<h4>Ok, so here would be the title of the task</h4>
+							<p>Be quiet, the adults are testing!!!</p>
+							<div class="information">
+								<div class="infoleft"><h4><small>Likes:15</small></h4></div>
+								<div class="infoleft"><h4><small>Comments:9</small></h4></div>
+								<div class="infoleft"><h4><small>Applied:5</small></h4></div>
+								<div class="inforight"><h4><small>Time: Yesterday</small></h4></div>
+							</div>
+						</div>
+					</li>
+					<li>
+						<div class="author"><img src="img/avatar.jpg" class="avatar img-rounded"></div>
+						<div class="content">
+							<h5>Name</h5>
+							<h4>Ok, so here would be the title of the task</h4>
+							<p>Be quiet, the adults are testing!!!</p>
+							<div class="information">
+								<div class="infoleft"><h4><small>Likes:15</small></h4></div>
+								<div class="infoleft"><h4><small>Comments:9</small></h4></div>
+								<div class="infoleft"><h4><small>Applied:5</small></h4></div>
+								<div class="inforight"><h4><small>Time: 2 Days Ago</small></h4></div>
+							</div>
+						</div>
+					</li>
+					<li>
+						<div class="author"><img src="img/avatar.jpg" class="avatar img-rounded"></div>
+						<div class="content">
+							<h5>Name</h5>
+							<h4>Ok, so here would be the title of the task</h4>
+							<p>Be quiet, the adults are testing!!!</p>
+							<div class="information">
+								<div class="infoleft"><h4><small>Likes:15</small></h4></div>
+								<div class="infoleft"><h4><small>Comments:9</small></h4></div>
+								<div class="infoleft"><h4><small>Applied:5</small></h4></div>
+								<div class="inforight"><h4><small>Time: 2 Days Ago</small></h4></div>
+							</div>
+						</div>
+					</li>
+					<li>
+						<div class="author"><img src="img/avatar.jpg" class="avatar img-rounded"></div>
+						<div class="content">
+							<h5>Name</h5>
+							<h4>Ok, so here would be the title of the task</h4>
+							<p>Be quiet, the adults are testing!!!</p>
+							<div class="information">
+								<div class="infoleft"><h4><small>Likes:15</small></h4></div>
+								<div class="infoleft"><h4><small>Comments:9</small></h4></div>
+								<div class="infoleft"><h4><small>Applied:5</small></h4></div>
+								<div class="inforight"><h4><small>Time: 2 Days Ago</small></h4></div>
+							</div>
+						</div>
+					</li>
+				</ul>
+				<div style="text-align:center">
+					<ul class="pagination">
+					  <li><a href="#">&laquo;</a></li>
+					  <li><a href="#">1</a></li>
+					  <li><a href="#">2</a></li>
+					  <li><a href="#">3</a></li>
+					  <li><a href="#">4</a></li>
+					  <li><a href="#">5</a></li>
+					  <li><a href="#">&raquo;</a></li>
+					</ul>
+				</div>
+			</div>
+			<div id="rightbar" class="col-md-3">
+			</div>
+		</div>
+	</div>
 	
 </body>
 </html>
