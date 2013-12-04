ad5b9182520217c6b1775850b62ec23cc5f30a4f
SherlocQ <chengchangqian@gmail.com>
2013-12-03 22:26:16 -0500

    Minor change to the nav bar

--- header.php
+++ header.php
@@ -1,53 +1,42 @@
 <nav class="navbar navbar-default" role="navigation">	
 	<div class="container">
-	<!-- Navbar -->
-	  <div class="navbar-header">
-	    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-	      <span class="sr-only">Toggle navigation</span>
-	      <span class="icon-bar"></span>
-	      <span class="icon-bar"></span>
-	      <span class="icon-bar"></span>
-	    </button>
-	    <a class="navbar-brand" href="#">MHelper</a>
-	  </div>
+		<!-- Navbar -->
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="#">MHelper</a>
+		</div>
 
-	  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-	    <!-- <ul class="nav navbar-nav">
-	      <li class="active"><a href="#">Link</a></li>
-	      <li><a href="#">Link</a></li>
-	      <li class="dropdown">
-	        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-	        <ul class="dropdown-menu">
-	          <li><a href="#">Action</a></li>
-	          <li><a href="#">Another action</a></li>
-	          <li><a href="#">Something else here</a></li>
-	          <li class="divider"></li>
-	          <li><a href="#">Separated link</a></li>
-	          <li class="divider"></li>
-	          <li><a href="#">One more separated link</a></li>
-	        </ul>
-	      </li>
-	    </ul> -->
-	    <form class="navbar-form navbar-left" role="search">
-	      <div class="form-group">
-	        <input type="text" id="searchbox" class="form-control" placeholder="Search">
-	      </div>
-	      <button type="submit" class="btn btn-default">Help</button>
-	    </form>
-	    <ul class="nav navbar-nav navbar-right">
-	      <!-- <li><a href="#">Link</a></li> -->
-		  <li><img src="img/avatar.jpg" class="avatar"></li>
-	      <li class="dropdown">
-	        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Chengqi Zhu<b class="caret"></b></a>
-	        <ul class="dropdown-menu">
-	          <li><a href="#">Action</a></li>
-	          <li><a href="#">Another action</a></li>
-	          <li><a href="#">Something else here</a></li>
-	          <li class="divider"></li>
-	          <li><a href="#">Separated link</a></li>
-	        </ul>
-	      </li>
-	    </ul>
-	  </div><!-- /.navbar-collapse -->
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+			<ul class="nav navbar-nav">
+				<li class="active"><a href="#"><i class="fa fa-home"></i> Explore</a></li>
+			</ul>
+
+			<form class="navbar-form navbar-left" id="searchBar" role="search">
+				<div class="form-group">
+					<input type="text" id="searchBox" class="form-control" placeholder="Search">
+				</div>
+				<button type="submit" class="btn btn-default" id="searchBtn"><i class="fa fa-search"></i></button>
+			</form>
+
+			<ul class="nav navbar-nav navbar-right">
+				<li><img src="img/avatar.jpg" class="avatar"></li>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Chengqi Zhu<b class="caret"></b></a>
+					<ul class="dropdown-menu">
+						<li><a href="#"><i class="fa fa-user" style="margin-right:10px;"></i>Profile</a></li>
+						<li><a href="#"><i class="fa fa-cog" style="margin-right:10px;"></i>Setting</a></li>
+						<li class="divider"></li>
+						<li><a href="#"><i class="fa fa-sign-out" style="margin-right:10px;"></i>Logout</a></li>
+					</ul>
+				</li>
+			</ul>
+
+		</div><!-- /.navbar-collapse -->
 	</div>
 </nav>
