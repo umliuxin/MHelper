562ffb5a35d49aed79f80741d865086762cf16ad
leonatefm <chengqizhufm@gmail.com>
2013-12-03 19:22:51 -0500

    filter title & search box width

--- header.php
+++ header.php
@@ -30,9 +30,9 @@
 	    </ul> -->
 	    <form class="navbar-form navbar-left" role="search">
 	      <div class="form-group">
-	        <input type="text" class="form-control" placeholder="Search">
+	        <input type="text" id="searchbox" class="form-control" placeholder="Search">
 	      </div>
-	      <button type="submit" class="btn btn-default">Submit</button>
+	      <button type="submit" class="btn btn-default">Help</button>
 	    </form>
 	    <ul class="nav navbar-nav navbar-right">
 	      <!-- <li><a href="#">Link</a></li> -->
