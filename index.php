552df757723fa3f5d38c2b14d8c33c0fa01bd72f
leonatefm <chengqizhufm@gmail.com>
2013-12-03 19:14:00 -0500

    awesome font

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 	<link rel="stylesheet" href="css/reset.min.css">
 	<link rel="stylesheet" href="css/index.css">
 	<link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
 	
 	<!-- Load Script Files -->
 	<script src="js/jquery/jquery.min.js"></script>
@@ -20,10 +21,12 @@
 	<div class="container">
 		<div class="row">
 			<div id="leftbar" class="col-md-2">
+				<h4><i class="fa fa-tags fa-fw"></i> Task Filter</h4>
 			</div>
 			<div id="mainbar" class="col-md-7">
-				<h3>Task Explore</h3>
+				<h4><i class="fa fa-th-list fa-fw"></i> Task Explore</h4>
 				<ul id="tasklist" class="list-unstyled">
+					<?php for($i=0; $i <= 10; $i++){?>
 					<li>
 						<div class="author"><img src="img/avatar.jpg" class="avatar img-rounded"></div>
 						<div class="content">
@@ -31,83 +34,14 @@
 							<h4>Ok, so here would be the title of the task</h4>
 							<p>Be quiet, the adults are testing!!!</p>
 							<div class="information">
-								<div class="infoleft"><h4><small>Likes:15</small></h4></div>
-								<div class="infoleft"><h4><small>Comments:9</small></h4></div>
-								<div class="infoleft"><h4><small>Applied:5</small></h4></div>
-								<div class="inforight"><h4><small>Time: Just Now</small></h4></div>
+								<div class="infoleft"><h4><small><i class="fa fa-thumbs-o-up"></i> 15</small></h4></div>
+								<div class="infoleft"><h4><small><i class="fa fa-comment-o"></i> 9</small></h4></div>
+								<div class="infoleft"><h4><small><i class="fa fa-rocket"></i> 5</small></h4></div>
+								<div class="inforight"><h4><small><i class="fa fa-clock-o"></i> Just Now</small></h4></div>
 							</div>
 						</div>
 					</li>
-					<li>
-						<div class="author"><img src="img/avatar.jpg" class="avatar img-rounded"></div>
-						<div class="content">
-							<h5>Name</h5>
-							<h4>Ok, so here would be the title of the task</h4>
-							<p>Be quiet, the adults are testing!!!</p>
-							<div class="information">
-								<div class="infoleft"><h4><small>Likes:15</small></h4></div>
-								<div class="infoleft"><h4><small>Comments:9</small></h4></div>
-								<div class="infoleft"><h4><small>Applied:5</small></h4></div>
-								<div class="inforight"><h4><small>Time: Yesterday</small></h4></div>
-							</div>
-						</div>
-					</li>
-					<li>
-						<div class="author"><img src="img/avatar.jpg" class="avatar img-rounded"></div>
-						<div class="content">
-							<h5>Name</h5>
-							<h4>Ok, so here would be the title of the task</h4>
-							<p>Be quiet, the adults are testing!!!</p>
-							<div class="information">
-								<div class="infoleft"><h4><small>Likes:15</small></h4></div>
-								<div class="infoleft"><h4><small>Comments:9</small></h4></div>
-								<div class="infoleft"><h4><small>Applied:5</small></h4></div>
-								<div class="inforight"><h4><small>Time: Yesterday</small></h4></div>
-							</div>
-						</div>
-					</li>
-					<li>
-						<div class="author"><img src="img/avatar.jpg" class="avatar img-rounded"></div>
-						<div class="content">
-							<h5>Name</h5>
-							<h4>Ok, so here would be the title of the task</h4>
-							<p>Be quiet, the adults are testing!!!</p>
-							<div class="information">
-								<div class="infoleft"><h4><small>Likes:15</small></h4></div>
-								<div class="infoleft"><h4><small>Comments:9</small></h4></div>
-								<div class="infoleft"><h4><small>Applied:5</small></h4></div>
-								<div class="inforight"><h4><small>Time: 2 Days Ago</small></h4></div>
-							</div>
-						</div>
-					</li>
-					<li>
-						<div class="author"><img src="img/avatar.jpg" class="avatar img-rounded"></div>
-						<div class="content">
-							<h5>Name</h5>
-							<h4>Ok, so here would be the title of the task</h4>
-							<p>Be quiet, the adults are testing!!!</p>
-							<div class="information">
-								<div class="infoleft"><h4><small>Likes:15</small></h4></div>
-								<div class="infoleft"><h4><small>Comments:9</small></h4></div>
-								<div class="infoleft"><h4><small>Applied:5</small></h4></div>
-								<div class="inforight"><h4><small>Time: 2 Days Ago</small></h4></div>
-							</div>
-						</div>
-					</li>
-					<li>
-						<div class="author"><img src="img/avatar.jpg" class="avatar img-rounded"></div>
-						<div class="content">
-							<h5>Name</h5>
-							<h4>Ok, so here would be the title of the task</h4>
-							<p>Be quiet, the adults are testing!!!</p>
-							<div class="information">
-								<div class="infoleft"><h4><small>Likes:15</small></h4></div>
-								<div class="infoleft"><h4><small>Comments:9</small></h4></div>
-								<div class="infoleft"><h4><small>Applied:5</small></h4></div>
-								<div class="inforight"><h4><small>Time: 2 Days Ago</small></h4></div>
-							</div>
-						</div>
-					</li>
+					<?php }?>
 				</ul>
 				<div style="text-align:center">
 					<ul class="pagination">
