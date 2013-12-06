38bbc8ef7d23391b3507df7f8ea79a5fad724e31
SherlocQ <chengchangqian@gmail.com>
2013-12-06 15:25:17 -0500

    Create tab pane & more content

--- profile.php
+++ profile.php
@@ -59,40 +59,69 @@
 
 		<div class="row" class="profile-content">
 			<div id="profile-tasks" class="col-md-7 col-md-offset-1">
-				<ul class="nav nav-pills">
-      				<li class="active"><a href="#" data-toggle="tab"><small>Created Tasks</small></a></li>
-      				<li><a href="#" data-toggle="tab"><small>Helped Tasks</small></a></li>
-					<li><a href="#" data-toggle="tab"><small>Endorsed Skills</small></a></li>
-					<li><a href="#" data-toggle="tab"><small>Reputation</small></a></li>     				
-    				<li><a href="#" data-toggle="tab"><small>Friend List</small></a></li>
+				<ul class="nav nav-pills" id="myTab">
+      				<li class="active"><a href="#created" data-toggle="tab"><small>Created Tasks  <span class="badge">5</span></small></a></li>
+      				<li><a href="#helped" data-toggle="tab"><small>Helped Tasks <span class="badge">10</span></small></a></li>
+					<li><a href="#endorse" data-toggle="tab"><small>Skills &amp; Expertise</small></a></li>
+					<li><a href="#reputation" data-toggle="tab"><small>Reputation</small></a></li>     				
+    				<li><a href="#friendlist" data-toggle="tab"><small>Friend List</small></a></li>
     			</ul>
-				<ul id="tasklist" class="list-unstyled">
-					<?php for($i=0; $i <= 10; $i++){?>
-					<li>
-						<div class="author"><img src="img/avatar.jpg" class="avatar img-rounded"></div>
-						<div class="content">
-							<h5>Name, <span class="text-muted">I wrote python, I'm a super hero.</span></h5>
-							<h4>Ok, so here would be the title of the task</h4>
-							<p>Hey, be quiet, the adults are testing.</p>
-							<div class="information">
-								<div class="infoleft"><h4><small><i class="fa fa-thumbs-o-up"></i> 15</small></h4></div>
-								<div class="infoleft"><h4><small><i class="fa fa-comment-o"></i> 9</small></h4></div>
-								<div class="infoleft"><h4><small><i class="fa fa-rocket"></i> 5</small></h4></div>
-								<div class="inforight"><h4><small><i class="fa fa-clock-o"></i> Just Now</small></h4></div>
+
+				<div class="tab-content">
+				  <div class="tab-pane active" id="created">
+					<ul id="tasklist" class="list-unstyled">
+						<?php for($i=0; $i <= 4; $i++){?>
+						<li>
+							<div class="author"><img src="img/avatar.jpg" class="avatar img-rounded"></div>
+							<div class="content">
+								<h5>Name, <span class="text-muted">I wrote python, I'm a super hero.</span></h5>
+								<h4>Ok, so here would be the title of the task</h4>
+								<p>Hey, be quiet, the adults are testing.</p>
+								<div class="information">
+									<div class="infoleft"><h4><small><i class="fa fa-thumbs-o-up"></i> 15</small></h4></div>
+									<div class="infoleft"><h4><small><i class="fa fa-comment-o"></i> 9</small></h4></div>
+									<div class="infoleft"><h4><small><i class="fa fa-rocket"></i> 5</small></h4></div>
+									<div class="inforight"><h4><small><i class="fa fa-clock-o"></i> Just Now</small></h4></div>
+								</div>
 							</div>
-						</div>
-					</li>
-					<?php }?>
-				</ul>
+						</li>
+						<?php }?>
+					</ul>
+				  	</div>
+				  	<div class="tab-pane" id="helped">
+						<ul id="tasklist" class="list-unstyled">
+							<?php for($i=0; $i <= 9; $i++){?>
+							<li>
+								<div class="author"><img src="img/avatar.jpg" class="avatar img-rounded"></div>
+								<div class="content">
+									<h5>Name, <span class="text-muted">I wrote python, I'm a super hero.</span></h5>
+									<h4>Ok, so here would be the title of the task</h4>
+									<p>Hey, be quiet, the adults are testing.</p>
+									<div class="information">
+										<div class="infoleft"><h4><small><i class="fa fa-thumbs-o-up"></i> 15</small></h4></div>
+										<div class="infoleft"><h4><small><i class="fa fa-comment-o"></i> 9</small></h4></div>
+										<div class="infoleft"><h4><small><i class="fa fa-rocket"></i> 5</small></h4></div>
+										<div class="inforight"><h4><small><i class="fa fa-clock-o"></i> Just Now</small></h4></div>
+									</div>
+								</div>
+							</li>
+							<?php }?>
+						</ul>				  		
+				  	</div>
+				  	<div class="tab-pane" id="endorse">endorse content</div>
+				  	<div class="tab-pane" id="reputation">reputation content</div>
+					<div class="tab-pane" id="friendlist">friendlist content</div>			  
+				</div>
+
 			</div>
-		</div>
-
-
-	</div>
 
 	<script>
-		$(document).ready(function() {
 
+		$(document).ready(function() {
+			$('#myTab a').click(function (e) {
+			  e.preventDefault();
+			  $(this).tab('show');
+			})
 		});
 	</script>
 </body>
