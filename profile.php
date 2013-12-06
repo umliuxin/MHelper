a62b1804c3efb17de162821e23b5cd89942a37f1
SherlocQ <chengchangqian@gmail.com>
2013-12-06 00:47:01 -0500

    Create avatar part

--- profile.php
+++ profile.php
@@ -17,5 +17,26 @@
 </head>
 <body>
 	<?php require_once("header.php"); ?>
-	
+	<div class="container">
+		<div class="row" id="profile">
+			<div id="profile-content" class="col-md-9">
+				<div class="row">
+					<div class="col-sm-2 col-md-2 no-padding">
+						<img src="img/avatar.jpg" class="profile-avatar img-rounded img-responsive">
+	        		</div>
+	        		<div class="col-sm-4 col-md-4 no-padding">
+	            		<blockquote>
+	                		<p>Joe Doe</p>
+	                		<small>Ann Arbor, United States  <i class="fa fa-map-marker"></i></small>
+		            		<small>joedoe@gmail.com <i class="fa fa-envelope"></i></small>
+		            		<small>www.mhelper.com <i class="fa fa-globe"></i></small>
+		            		<small>January 30, 1974 <i class="fa fa-gift"></i></small>
+		                </blockquote>
+	        		</div>
+	        	</div>
+            </div>
+			<div id="profile-sidebar" class="col-md-3">
+			</div>
+		</div>
+	</div>
 </body>
