0c80338dca4333610b014dc4f162aa61522461d6
SherlocQ <chengchangqian@gmail.com>
2013-12-06 12:07:25 -0500

    adjust profile avatar, add statistic info

--- profile.php
+++ profile.php
@@ -20,23 +20,48 @@
 	<div class="container">
 		<div class="row" id="profile">
 			<div id="profile-content" class="col-md-9">
-				<div class="row">
-					<div class="col-sm-2 col-md-2 no-padding">
-						<img src="img/avatar.jpg" class="profile-avatar img-rounded img-responsive">
-	        		</div>
-	        		<div class="col-sm-4 col-md-4 no-padding">
-	            		<blockquote>
-	                		<p>Joe Doe</p>
+				
+				<div class="media">
+            		<a class="pull-left" href="#">
+            			<img class="profile-avatar media-object dp img-circle" src="img/avatar.jpg" >
+            		</a>
+		            <div class="media-body col-md-6">
+		                <h4 class="media-heading">Joe Doe <small> Ann Arbor</small></h4>
+		               	<blockquote style="margin:0;">
 	                		<small>Ann Arbor, United States  <i class="fa fa-map-marker"></i></small>
 		            		<small>joedoe@gmail.com <i class="fa fa-envelope"></i></small>
 		            		<small>www.mhelper.com <i class="fa fa-globe"></i></small>
-		            		<small>January 30, 1974 <i class="fa fa-gift"></i></small>
 		                </blockquote>
-	        		</div>
-	        	</div>
-            </div>
+		                <hr style="margin:8px auto">
+		                <span class="label label-default">HTML5/CSS3</span>
+		                <span class="label label-primary">jQuery</span>
+		                <span class="label label-info">Graphic Design</span>
+		                <span class="label label-success">Python</span>
+		            </div>
+        		</div>
+	        </div>
+			
 			<div id="profile-sidebar" class="col-md-3">
-			</div>
+				<div class="list-group">
+	                <a href="#" class="list-group-item create">
+	                    <h3 class="pull-right"><i class="fa fa-big fa-edit"></i></h3>
+	                    <h4 class="list-group-item-heading count">5</h4>
+	                    <p class="list-group-item-text">Created Tasks</p>
+	                </a>
+	                <a href="#" class="list-group-item help">
+	                    <h3 class="pull-right"><i class="fa fa-big fa-smile-o"></i></h3>
+	                    <h4 class="list-group-item-heading count">10</h4>
+	                    <p class="list-group-item-text">Helped Tasks</p>
+	                </a>	                	                
+            	</div>				
+        	</div>
+
 		</div>
 	</div>
+
+	<script>
+		$(document).ready(function() {
+
+		});
+	</script>
 </body>
