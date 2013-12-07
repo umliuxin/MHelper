794ae2b17938ddfefe81a85e9ed922f837b6815d
leonatefm <chengqizhufm@gmail.com>
2013-12-07 17:35:55 -0500

    more contents on task detail

--- task.php
+++ task.php
@@ -21,10 +21,52 @@
 		<div class="row">
 			<div id="task-content" class="col-md-6 col-md-offset-1">
 				<h3>Help needed on laser cutting</h3>
-				<h4>Task Description</h4>
-				<p>I’m implementing a game involving physical interaction on a turntable. With no experience of engineering or handcrafting, I have no idea how to make it but I heard that laser cutting can be a good way, so I’m hoping someone can help with it.
+				<div class="authorinfo">
+					<span class="pull-left"><img class="avatar" src="img/superhero/Hawkeye.png">Felicia</span>
+					<span class="pull-right">3 Hours Ago</span>
+				</div>
+				<ul class="list-unstyled">
+					<li>
+						<h4>Task Description</h4>
+						<p>I’m implementing a game involving physical interaction on a turntable. With no experience of engineering or handcrafting, I have no idea how to make it but I heard that laser cutting can be a good way, so I’m hoping someone can help with it.
  
-What I need is an 18’’ round board of any solid material, maybe plywood or plastics. It should be horizontally rotatable around a shaft at the center smoothly and steadily. In the game, the player will be turning the board to control, so the edge may need to be milled smoother. A platform is needed as the base for the shaft. There should be free space at least 1.8’’ in height between the rotatable board and the platform for placing sensors later.</p>
+		What I need is an 18’’ round board of any solid material, maybe plywood or plastics. It should be horizontally rotatable around a shaft at the center smoothly and steadily. In the game, the player will be turning the board to control, so the edge may need to be milled smoother. A platform is needed as the base for the shaft. There should be free space at least 1.8’’ in height between the rotatable board and the platform for placing sensors later.</p>
+					</li>
+					<li>
+						<h4>Skills Requirements</h4>
+						<button type="button" class="btn btn-warning">laser cutting</button>
+						<button type="button" class="btn btn-info">Adobe Illustrator</button>
+					</li>
+					<li>
+						<h4>Applicants(3)</h4>
+						<img class="avatar" src="img/superhero/Hawkeye.png" title="changge"><img class="avatar" src="img/superhero/Black Widow.png"><img class="avatar" src="img/superhero/Hulk.png">
+					</li>
+					<li>
+						<h4>Message Board</h4>
+						<ul id="messages" class="list-unstyled">
+							<?php for($i=0; $i <= 4; $i++){?>
+							<li>
+								<div class="author"><img src="img/avatar.jpg" class="avatar img-rounded"></div>
+								<div class="content">
+									<h5>Name, <span class="text-muted">I wrote python, I'm a super hero.</span><span class="time pull-right">3 mins ago</span></h5>
+									<p>Hey, be quiet, the adults are testing.</p>
+								</div>
+							</li>
+							<?php }?>
+							<li>
+								<div class="author"><img src="img/avatar.jpg" class="avatar img-rounded"></div>
+								<div class="content">
+									<div class="form-group">
+										<textarea id="comment" class="form-control" rows="3" placeholder="Type in your message"></textarea>
+										<button type="button" class="btn btn-success pull-right">Comment</button>
+										<div class="checkbox"><label><input type="checkbox"> Private</label></div>
+									</div>
+								</div>
+							</li>
+						</ul>
+					</li>
+				</ul>
+				
 	        </div>
 			
 			<div id="task-sidebar" class="col-md-4">
