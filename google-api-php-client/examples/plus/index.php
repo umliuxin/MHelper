af77c11d918d7f747e66382e4201a0e2c4447593
SherlocQ <chengchangqian@gmail.com>
2013-12-07 21:45:27 -0500

    Google API

--- google-api-php-client/examples/plus/index.php
+++ google-api-php-client/examples/plus/index.php
@@ -1,17 +1,32 @@
 <?php
-require_once 'google-api-php-client/src/Google_Client.php';
-require_once 'google-api-php-client/src/contrib/Google_PlusService.php';
+/*
+ * Copyright 2011 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+require_once '../../src/Google_Client.php';
+require_once '../../src/contrib/Google_PlusService.php';
 
 session_start();
 
 $client = new Google_Client();
-$client->setApplicationName("MHelper");
+$client->setApplicationName("Google+ PHP Starter Application");
 // Visit https://code.google.com/apis/console to generate your
 // oauth2_client_id, oauth2_client_secret, and to register your oauth2_redirect_uri.
-$client->setClientId('965773967185-b3hd6glq9pi4vuorlo0kc21iuoh11n2s.apps.googleusercontent.com');
-$client->setClientSecret('IGcSNMAc5x0J_mXSDk8t3uL_');
-$client->setRedirectUri('http://localhost:8888/Github/MHelper/index.php');
-$client->setDeveloperKey('965773967185');
+// $client->setClientId('insert_your_oauth2_client_id');
+// $client->setClientSecret('insert_your_oauth2_client_secret');
+// $client->setRedirectUri('insert_your_oauth2_redirect_uri');
+// $client->setDeveloperKey('insert_your_developer_key');
 $plus = new Google_PlusService($client);
 
 if (isset($_REQUEST['logout'])) {
@@ -56,40 +71,31 @@ if ($client->getAccessToken()) {
   $authUrl = $client->createAuthUrl();
 }
 ?>
-
 <!doctype html>
-<html lang="en">
+<html>
 <head>
-  <meta charset="UTF-8">
-  <title>MHelper-Profile</title>
-  
-  <!-- Load CSS Files -->
-  <link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
-  <link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
-  <link rel="stylesheet" href="css/login.css">
-  
-  <!-- Load Script Files -->
-  <script src="js/jquery/jquery.min.js"></script>
-  <script src="js/jquery/jquery-ui.min.js"></script>
-  <script src="js/bootstrap/bootstrap.min.js"></script>
-  
+  <meta charset="utf-8">
+  <link rel='stylesheet' href='style.css' />
 </head>
 <body>
-  <div id="fullscreen_bg" class="fullscreen_bg"/>
+<header><h1>Google+ Sample App</h1></header>
+<div class="box">
 
-  <div class="container">
-    <form class="form-signin">
-      <h1 class="form-signin-heading text-muted">MHelper</h1>
-      <img class="form-signin-logo center-block" src="img/superhero/Iron Man.png" alt="logo">
-        <?php
-          if(isset($authUrl)) {
-            print "<a class='login btn btn-lg btn-primary btn-block' id='loginBtn' href='$authUrl'>Sign In</a>";
-          } else {
-            print "<a class='logout btn btn-lg btn-primary btn-block' id='loginBtn' href='?logout'>Logout</a>";
-          }
-        ?>
-      <div class="form-signin-text"><a href="index.php">Sign in with your umich unique name</a></div>
-    </form>
-  </div>
+<?php if(isset($personMarkup)): ?>
+<div class="me"><?php print $personMarkup ?></div>
+<?php endif ?>
+
+<?php if(isset($activityMarkup)): ?>
+<div class="activities">Your Activities: <?php print $activityMarkup ?></div>
+<?php endif ?>
+
+<?php
+  if(isset($authUrl)) {
+    print "<a class='login' href='$authUrl'>Connect Me!</a>";
+  } else {
+   print "<a class='logout' href='?logout'>Logout</a>";
+  }
+?>
+</div>
 </body>
 </html>
